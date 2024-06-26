09833cc41492753ffbe5ec0523d1552a98eb9509
Mansoor-P <munaf6007@gmail.com>
2024-06-26 19:52:22 +0530

    Project Hotel Booking Setup

---
+++ src/test/java/com/mansoor/hotelbooking/HotelBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mansoor.hotelbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
